7560615ecb89f50f11677f93c285c02ec691398d
JuanNeiraJaime <l17100264@nlared.tecnm.mx>
2022-10-06 06:51:14 -0600

    Primeros pasos en kotlin

--- app/src/main/java/com/example/baseskotlin/Principal.kt
+++ app/src/main/java/com/example/baseskotlin/Principal.kt
@@ -6,4 +6,9 @@ fun main(){
 
 fun newTopic(topic: String){
     println("==================== $topic ====================")
+
+    val a: Int = 7 // delcaracion de constante con su tipo de dato
+
+    //declaracion de variable
+    var b = 14
 }
