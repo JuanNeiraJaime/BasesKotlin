425d4dedb18971548fd20739f65b7ad8af72c5f7
JuanNeiraJaime <l17100264@nlared.tecnm.mx>
2022-10-06 06:42:58 -0600

    Primeros pasos en kotlin

---
+++ app/src/main/java/com/example/baseskotlin/Principal.kt
@@ -0,0 +1,9 @@
+package com.example.baseskotlin
+
+fun main(){
+    newTopic("Primeros pasos en kotlin")
+}
+
+fun newTopic(topic: String){
+    println("==================== $topic ====================")
+}
